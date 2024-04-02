6f792ddbe3a6a31a00536f3def7ebd0a207f0d23
jimmysingapore <52878605+jimmysingapore@users.noreply.github.com>
2024-04-02 11:39:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ def i():
         num = float(request.form.get("rates"))
         return(render_template("index.html", result = 90.2-(50.6*num)))
     else:
+        time.sleep(60)
         return render_template("index.html", result ="Waiting……….")
 if __name__=="__main__":
     app.run()
